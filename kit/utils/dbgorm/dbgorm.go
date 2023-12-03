5a55d7c45ffd897a00a5108cda711da25f6b5a8e
thanhld <thanhld9x.dev@gmail.com>
2023-12-03 16:46:29 +0700

    udpate base model

--- kit/utils/dbgorm/dbgorm.go
+++ kit/utils/dbgorm/dbgorm.go
@@ -1,9 +1,24 @@
 package dbgorm
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
-type Model struct {
-	ID        int64     `json:"id" gorm:"primarykey"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+type BaseModel struct {
+	gorm.Model
+	ID        int64          `json:"id" gorm:"primarykey"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+type TrackingBaseModel struct {
+	gorm.Model
+	ID            int64          `json:"id" gorm:"primarykey"`
+	CreatedAt     time.Time      `json:"created_at"`
+	CreatedUserID int64          `json:"created_user_id"`
+	UpdatedAt     time.Time      `json:"updated_at"`
+	UpdatedUserID int64          `json:"updated_user_id"`
+	DeletedAt     gorm.DeletedAt `gorm:"index"`
 }
